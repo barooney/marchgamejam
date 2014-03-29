62bc2061ac839ff3bc63c5f8c9ed0807de574040
oli <oliver.stadie@gmail.com>
2014-03-29 19:23:04 +0100

    added castle.isInside(x,y) and cleaned up.

--- castle.js
+++ castle.js
@@ -3,36 +3,36 @@ var castle = function() {
 		posX : 50,
 		posY : 50,
 		neighbours : [],
-		owner : 1, 
+		owner : 1,
 
 		drawMe : function() {
 			switch (this.owner) {
 			case 1:
-			ctx.beginPath();
-			ctx.lineWidth = 0.1;
-			ctx.fillStyle = "rgba(255,0,0,0.5)";
-			ctx.arc( this.posX, this.posY, 28, 00,Math.PI*2,false);
-			ctx.fill();
-			ctx.stroke();
-			break;
+				ctx.beginPath();
+				ctx.lineWidth = 0.1;
+				ctx.fillStyle = "rgba(255,0,0,0.5)";
+				ctx.arc(this.posX, this.posY, 28, 00, Math.PI * 2, false);
+				ctx.fill();
+				ctx.stroke();
+				break;
 			case 2:
-			ctx.fillStyle = "#0F0";
-			ctx.fillRect( this.posX - 20, this.posY - 20, 40, 40);
-			break;
+				ctx.fillStyle = "#0F0";
+				ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
+				break;
 			case 3:
-			ctx.fillStyle = "#00F";
-			ctx.fillRect( this.posX - 20, this.posY - 20, 40, 40);
-			break;
+				ctx.fillStyle = "#00F";
+				ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
+				break;
 			}
-			
+
 			// draw the castle itself
 			ctx.drawImage(castleImg, this.posX - 20, this.posY - 20);
 		},
-	
+
 		drawStreets : function() {
 			// draw the way to the neighbors
 			ctx.strokeStyle = "#999";
-			ctx.lineWidth=10;
+			ctx.lineWidth = 10;
 			for (neighboursIndex in this.neighbours) {
 				var curNeighbours = this.neighbours[neighboursIndex];
 				ctx.beginPath();
@@ -40,6 +40,11 @@ var castle = function() {
 				ctx.lineTo(curNeighbours.posX, curNeighbours.posY);
 				ctx.stroke();
 			}
+		},
+
+		isInside : function(x, y) {
+			return (x >= this.posX - 20) && (x <= this.posX + 20)
+					&& (y > this.posY - 20) && (y < this.posY + 20);
 		}
 	};
 };
