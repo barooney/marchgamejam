10fc4018e563847e1c60ef04c0661dd7cf1ab98c
Daniel Baron <daniel@barooney.de>
2014-03-29 19:48:11 +0100

    added background image

--- castle.js
+++ castle.js
@@ -1,50 +1,49 @@
 var castle = function() {
-	return {
-		posX : 50,
-		posY : 50,
-		neighbours : [],
-		owner : 1,
+    return {
+        posX: 50,
+        posY: 50,
+        neighbours: [],
+        owner: 1,
 
-		drawMe : function() {
-			switch (this.owner) {
-			case 1:
-				ctx.beginPath();
-				ctx.lineWidth = 0.1;
-				ctx.fillStyle = "rgba(255,0,0,0.5)";
-				ctx.arc(this.posX, this.posY, 28, 00, Math.PI * 2, false);
-				ctx.fill();
-				ctx.stroke();
-				break;
-			case 2:
-				ctx.fillStyle = "#0F0";
-				ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
-				break;
-			case 3:
-				ctx.fillStyle = "#00F";
-				ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
-				break;
-			}
+        drawMe: function() {
+            switch (this.owner) {
+                case 1:
+                    ctx.beginPath();
+                    ctx.lineWidth = 0.1;
+                    ctx.fillStyle = "rgba(255,0,0,0.5)";
+                    ctx.arc(this.posX, this.posY, 28, 00, Math.PI * 2, false);
+                    ctx.fill();
+                    ctx.stroke();
+                    break;
+                case 2:
+                    ctx.fillStyle = "#0F0";
+                    ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
+                    break;
+                case 3:
+                    ctx.fillStyle = "#00F";
+                    ctx.fillRect(this.posX - 20, this.posY - 20, 40, 40);
+                    break;
+            }
 
-			// draw the castle itself
-			ctx.drawImage(castleImg, this.posX - 20, this.posY - 20);
-		},
+            // draw the castle itself
+            ctx.drawImage(castleImg, this.posX - 20, this.posY - 20);
+        },
 
-		drawStreets : function() {
-			// draw the way to the neighbors
-			ctx.strokeStyle = "#999";
-			ctx.lineWidth = 10;
-			for (neighboursIndex in this.neighbours) {
-				var curNeighbours = this.neighbours[neighboursIndex];
-				ctx.beginPath();
-				ctx.moveTo(this.posX, this.posY);
-				ctx.lineTo(curNeighbours.posX, curNeighbours.posY);
-				ctx.stroke();
-			}
-		},
+        drawStreets: function() {
+            // draw the way to the neighbors
+            ctx.strokeStyle = "#999";
+            ctx.lineWidth = 10;
+            for (neighboursIndex in this.neighbours) {
+                var curNeighbours = this.neighbours[neighboursIndex];
+                ctx.beginPath();
+                ctx.moveTo(this.posX, this.posY);
+                ctx.lineTo(curNeighbours.posX, curNeighbours.posY);
+                ctx.stroke();
+            }
+        },
 
-		isInside : function(x, y) {
-			return (x >= this.posX - 20) && (x <= this.posX + 20)
-					&& (y > this.posY - 20) && (y < this.posY + 20);
-		}
-	};
+        isInside: function(x, y) {
+            return (x >= this.posX - 20) && (x <= this.posX + 20) && (y > this.posY - 20) && (y < this.posY + 20);
+        }
+    };
 };
