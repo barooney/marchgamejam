2270d1492d8ed40366a4a4f473fc568a1f60caae
Daniel Baron <daniel@barooney.de>
2014-03-29 18:06:05 +0100

    images for knight

--- soldier.js
+++ soldier.js
@@ -1,20 +1,18 @@
 var soldier = function() {
-	return {
-		startingCastle : null,
-		targetCastle : null,
-		progress : 0,
+    return {
+        startingCastle: null,
+        targetCastle: null,
+        progress: 0,
 
-		getPosX : function() {
-			return (1.0 - this.progress) * this.startingCastle.posX
-					+ (this.progress) * this.targetCastle.posX;
-		},
-		getPosY : function() {
-			return (1.0 - this.progress) * this.startingCastle.posY
-					+ (this.progress) * this.targetCastle.posY;
-		},
+        getPosX: function() {
+            return (1.0 - this.progress) * this.startingCastle.posX + (this.progress) * this.targetCastle.posX;
+        },
+        getPosY: function() {
+            return (1.0 - this.progress) * this.startingCastle.posY + (this.progress) * this.targetCastle.posY;
+        },
 
-		drawMe : function() {
-			ctx.drawImage(soldierImg, this.getPosX(), this.getPosY());
-		}
-	};
-}
+        drawMe: function() {
+            ctx.drawImage(soldierImgs[animFrame], this.getPosX(), this.getPosY());
+        }
+    };
+};
