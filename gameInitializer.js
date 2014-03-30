58d263d1ac89f5fe981ef3dd66282895b8100082
oli <oliver.stadie@gmail.com>
2014-03-30 16:33:25 +0200

    Cleaned up code.

---
+++ gameInitializer.js
@@ -0,0 +1,158 @@
+var FRAME_DELAY = 50; 
+
+var framesToNextSpawn1 = 20;
+var framesToNextSpawn2 = 20;
+var framesToNextSpawn3 = 20;
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var castleImg = new Image();
+castleImg.src = 'img/40x40castle.png';
+
+var startscreenImg = new Image();
+startscreenImg.src = 'img/startscreen.png';
+
+var way1Img = new Image();
+way1Img.src = 'img/way_1.png';
+var way2Img = new Image();
+way2Img.src = 'img/way_2.png';
+
+var backgroundImg = new Image();
+backgroundImg.src = 'img/background.png';
+
+var flagImgs1 = [ new Image(), new Image() ];
+var flagImgs2 = [ new Image(), new Image() ];
+var flagImgs3 = [ new Image(), new Image() ];
+flagImgs1[0].src = 'img/fahne_orange_1.png';
+flagImgs1[1].src = 'img/fahne_orange_2.png';
+flagImgs2[0].src = 'img/fahne_blau_1.png';
+flagImgs2[1].src = 'img/fahne_blau_2.png';
+flagImgs3[0].src = 'img/fahne_gruen_1.png';
+flagImgs3[1].src = 'img/fahne_gruen_2.png';
+
+var animFrame = 0;
+var animFrameCtr = 0;
+var animFrameMax = 5;
+
+var animFlagFrame = 0;
+var animFlagFrameMax = 1;
+var animFlagFrameCtr = 0;
+
+var soldierImgs1 = [ new Image(), new Image(), new Image ];
+var soldierImgs2 = [ new Image(), new Image(), new Image ];
+var soldierImgs3 = [ new Image(), new Image(), new Image ];
+soldierImgs1[0].src = 'img/knight_1orange.png';
+soldierImgs1[1].src = 'img/knight_2orange.png';
+soldierImgs1[2].src = 'img/knight_3orange.png';
+soldierImgs2[0].src = 'img/knight_1blau.png';
+soldierImgs2[1].src = 'img/knight_2blau.png';
+soldierImgs2[2].src = 'img/knight_3blau.png';
+soldierImgs3[0].src = 'img/knight_1gruen.png';
+soldierImgs3[1].src = 'img/knight_2gruen.png';
+soldierImgs3[2].src = 'img/knight_3gruen.png';
+
+var castles = [];
+for (castlePosIndex in castlePosArray) {
+	var curPosObj = castlePosArray[castlePosIndex];
+	var curCastle = new castle();
+	curCastle.posX = curPosObj.x;
+	curCastle.posY = curPosObj.y;
+	curCastle.owner = curPosObj.owner;
+	castles.push(curCastle);
+}
+// castles.reverse();
+
+// connect to neighbours
+for (castleIndex in castles) {
+	var curCastleNeighbourIndizes = castlePosArray[castleIndex].neighbours;
+	var curCastle = castles[castleIndex];
+
+	for (curCastleNeighbourIndizesIndex in curCastleNeighbourIndizes) {
+		var curNeighbourIndex = curCastleNeighbourIndizes[curCastleNeighbourIndizesIndex];
+		curCastle.neighbours.push(castles[curNeighbourIndex]);
+	}
+}
+
+var soldiers = [];
+
+var showStart = true;
+
+function init() {
+	framesToNextSpawn1 = 20;
+	framesToNextSpawn2 = 20;
+	framesToNextSpawn3 = 20;
+
+	c = document.getElementById("myCanvas");
+	ctx = c.getContext("2d");
+
+	castleImg = new Image();
+	castleImg.src = 'img/40x40castle.png';
+
+	startscreenImg = new Image();
+	startscreenImg.src = 'img/startscreen.png';
+
+	way1Img = new Image();
+	way1Img.src = 'img/way_1.png';
+	way2Img = new Image();
+	way2Img.src = 'img/way_2.png';
+
+	backgroundImg = new Image();
+	backgroundImg.src = 'img/background.png';
+
+	flagImgs1 = [ new Image(), new Image() ];
+	flagImgs2 = [ new Image(), new Image() ];
+	flagImgs3 = [ new Image(), new Image() ];
+	flagImgs1[0].src = 'img/fahne_orange_1.png';
+	flagImgs1[1].src = 'img/fahne_orange_2.png';
+	flagImgs2[0].src = 'img/fahne_blau_1.png';
+	flagImgs2[1].src = 'img/fahne_blau_2.png';
+	flagImgs3[0].src = 'img/fahne_gruen_1.png';
+	flagImgs3[1].src = 'img/fahne_gruen_2.png';
+
+	animFrame = 0;
+	animFrameCtr = 0;
+	animFrameMax = 5;
+
+	animFlagFrame = 0;
+	animFlagFrameMax = 1;
+	animFlagFrameCtr = 0;
+
+	soldierImgs1 = [ new Image(), new Image(), new Image ];
+	soldierImgs2 = [ new Image(), new Image(), new Image ];
+	soldierImgs3 = [ new Image(), new Image(), new Image ];
+	soldierImgs1[0].src = 'img/knight_1orange.png';
+	soldierImgs1[1].src = 'img/knight_2orange.png';
+	soldierImgs1[2].src = 'img/knight_3orange.png';
+	soldierImgs2[0].src = 'img/knight_1blau.png';
+	soldierImgs2[1].src = 'img/knight_2blau.png';
+	soldierImgs2[2].src = 'img/knight_3blau.png';
+	soldierImgs3[0].src = 'img/knight_1gruen.png';
+	soldierImgs3[1].src = 'img/knight_2gruen.png';
+	soldierImgs3[2].src = 'img/knight_3gruen.png';
+
+	castles = [];
+	for (castlePosIndex in castlePosArray) {
+		var curPosObj = castlePosArray[castlePosIndex];
+		var curCastle = new castle();
+		curCastle.posX = curPosObj.x;
+		curCastle.posY = curPosObj.y;
+		curCastle.owner = curPosObj.owner;
+		castles.push(curCastle);
+	}
+	// castles.reverse();
+
+	// connect to neighbours
+	for (castleIndex in castles) {
+		var curCastleNeighbourIndizes = castlePosArray[castleIndex].neighbours;
+		var curCastle = castles[castleIndex];
+
+		for (curCastleNeighbourIndizesIndex in curCastleNeighbourIndizes) {
+			var curNeighbourIndex = curCastleNeighbourIndizes[curCastleNeighbourIndizesIndex];
+			curCastle.neighbours.push(castles[curNeighbourIndex]);
+		}
+	}
+
+	soldiers = [];
+	showStart = true;
+}
--- main.js
+++ main.js
@@ -1,84 +1,10 @@
-var framesToNextSpawn1 = 20;
-var framesToNextSpawn2 = 20;
-var framesToNextSpawn3 = 20;
+init();
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
-var castleImg = new Image();
-castleImg.src = 'img/40x40castle.png';
-
-var startscreenImg = new Image();
-startscreenImg.src = 'img/startscreen.png';
-
-var way1Img = new Image();
-way1Img.src = 'img/way_1.png';
-var way2Img = new Image();
-way2Img.src = 'img/way_2.png';
-
-var backgroundImg = new Image();
-backgroundImg.src = 'img/background.png';
-
-var flagImgs1 = [ new Image(), new Image() ];
-var flagImgs2 = [ new Image(), new Image() ];
-var flagImgs3 = [ new Image(), new Image() ];
-flagImgs1[0].src = 'img/fahne_orange_1.png';
-flagImgs1[1].src = 'img/fahne_orange_2.png';
-flagImgs2[0].src = 'img/fahne_blau_1.png';
-flagImgs2[1].src = 'img/fahne_blau_2.png';
-flagImgs3[0].src = 'img/fahne_gruen_1.png';
-flagImgs3[1].src = 'img/fahne_gruen_2.png';
-
-var animFrame = 0;
-var animFrameCtr = 0;
-var animFrameMax = 5;
-
-var animFlagFrame = 0;
-var animFlagFrameMax = 1;
-var animFlagFrameCtr = 0;
-
-var soldierImgs1 = [ new Image(), new Image(), new Image ];
-var soldierImgs2 = [ new Image(), new Image(), new Image ];
-var soldierImgs3 = [ new Image(), new Image(), new Image ];
-soldierImgs1[0].src = 'img/knight_1orange.png';
-soldierImgs1[1].src = 'img/knight_2orange.png';
-soldierImgs1[2].src = 'img/knight_3orange.png';
-soldierImgs2[0].src = 'img/knight_1blau.png';
-soldierImgs2[1].src = 'img/knight_2blau.png';
-soldierImgs2[2].src = 'img/knight_3blau.png';
-soldierImgs3[0].src = 'img/knight_1gruen.png';
-soldierImgs3[1].src = 'img/knight_2gruen.png';
-soldierImgs3[2].src = 'img/knight_3gruen.png';
-
-var castles = [];
-for (castlePosIndex in castlePosArray) {
-	var curPosObj = castlePosArray[castlePosIndex];
-	var curCastle = new castle();
-	curCastle.posX = curPosObj.x;
-	curCastle.posY = curPosObj.y;
-	curCastle.owner = curPosObj.owner;
-	castles.push(curCastle);
-}
-// castles.reverse();
-
-// connect to neighbours
-for (castleIndex in castles) {
-	var curCastleNeighbourIndizes = castlePosArray[castleIndex].neighbours;
-	var curCastle = castles[castleIndex];
-
-	for (curCastleNeighbourIndizesIndex in curCastleNeighbourIndizes) {
-		var curNeighbourIndex = curCastleNeighbourIndizes[curCastleNeighbourIndizesIndex];
-		curCastle.neighbours.push(castles[curNeighbourIndex]);
-	}
+function getWinner() {
+	return 0;
 }
 
-var soldiers = [];
-
-var showStart = 1;
-
-fly();
-
-function fly() {
+function fly() {	
 	if (showStart) {
 		ctx.drawImage(startscreenImg, 0, 0);
 	} else {
@@ -155,22 +81,33 @@ function fly() {
 			newSoldier.owner = 3;
 			soldiers.push(newSoldier);
 		}
+		
+		var winner = getWinner();
+		
+		if (winner === 1) {
+//			console.log('winner', winner);
+			
+		} else if (winner > 1) {
+//			console.log('loser! winner:', winner);
+		}
 	}
+	setTimeout('fly()', FRAME_DELAY);
 }
-
-setInterval(fly, 50);
 
 $('canvas').click(function(e) {
 	if (showStart) {
-		showStart = 0;
+		showStart = false;
 	} else {
 
-		x = e.pageX - this.offsetLeft, y = e.pageY - this.offsetTop;
+		var x = e.pageX - this.offsetLeft;
+		var y = e.pageY - this.offsetTop;
 
-		for ( var c in castles) {
+		for (var c in castles) {
 			if (castles[c].isInside(x, y) && castles[c].owner === 1) {
 				castles[c].nextTarget();
 			}
 		}
 	}
 });
+
+fly();
