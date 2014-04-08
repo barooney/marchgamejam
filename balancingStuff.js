fc968613fb8ed965b53eca6fd07af0edc32c237c
oli <oliver.stadie@gmail.com>
2014-04-08 19:38:46 +0200

    Issue 20: Made balancing stuff configurable via HTML interface.

--- balancingStuff.js
+++ balancingStuff.js
@@ -4,9 +4,9 @@
 // - (SPAWN_INTERVAL_REDUCTION_PER_CASTLE * CastleCount)
 // 
 // Frames to spawn base value
-SPAWN_INTERVAL_BASE = 100;
+var SPAWN_INTERVAL_BASE = 100;
 // Interval reduction implied by each castle
-SPAWN_INTERVAL_REDUCTION_PER_CASTLE = 3;
+var SPAWN_INTERVAL_REDUCTION_PER_CASTLE = 3;
 // #######################
 
 // #######################
@@ -14,22 +14,44 @@ SPAWN_INTERVAL_REDUCTION_PER_CASTLE = 3;
 // + RandomValueBetween(0, SOLDIER_SPEED_BONUS);
 //
 // soldier base speed in "percent of one way per frame"
-SOLDIER_SPEED_BASE = 0.01;
+var SOLDIER_SPEED_BASE = 0.01;
 // soldier speed bonus maximum
-SOLDIER_SPEED_BONUS = 0.01;
+var SOLDIER_SPEED_BONUS = 0.01;
 // #######################
 
 // #######################
 // FIGHTING
 //
 // Shall soldiers suicide after attacking an enemy castle?
-SOLDIER_SUICIDE_AFTER_ENEMY_CASTLE_ATTACK = true;
+var SOLDIER_SUICIDE_AFTER_ENEMY_CASTLE_ATTACK = true;
 // Shall soldiers suicide after attacking a neutral castle?
-SOLDIER_SUICIDE_AFTER_NEUTRAL_CASTLE_ATTACK = true;
+var SOLDIER_SUICIDE_AFTER_NEUTRAL_CASTLE_ATTACK = true;
 // How likely is the successfull capturing of a neutral castle (0 to 1)?
-WIN_AGAINST_NEUTRAL_CASTLE_PROPABILITY = 1.0; 
+var WIN_AGAINST_NEUTRAL_CASTLE_PROPABILITY = 1.0;
 // How likely is the successfull capturing of an enemy castle (0 to 1)?
-WIN_AGAINST_ENEMY_CASTLE_PROPABILITY = 0.5;
+var WIN_AGAINST_ENEMY_CASTLE_PROPABILITY = 0.5;
 // How likely it to beat an enemy soldier with an own solider (0 to 1)?
-WIN_AGAINST_ENEMY_SOLDIER_PROPABILITY = 0.5;
+var WIN_AGAINST_ENEMY_SOLDIER_PROPABILITY = 0.5;
 // #######################
+
+// #######################
+// the functions to do balncing from HTML forms
+// #######################
+function applyHtmlBalancingValues() {
+	SPAWN_INTERVAL_BASE = parseInt(document
+	    .getElementById('SPAWN_INTERVAL_BASE').value);
+	SPAWN_INTERVAL_REDUCTION_PER_CASTLE = parseInt(document
+	    .getElementById('SPAWN_INTERVAL_REDUCTION_PER_CASTLE').value);
+	SOLDIER_SPEED_BASE = parseFloat(document.getElementById('SOLDIER_SPEED_BASE').value);
+	SOLDIER_SPEED_BONUS = parseFloat(document.getElementById('SOLDIER_SPEED_BONUS').value);
+	SOLDIER_SUICIDE_AFTER_ENEMY_CASTLE_ATTACK = document
+	    .getElementById('SOLDIER_SUICIDE_AFTER_ENEMY_CASTLE_ATTACK').checked;
+	SOLDIER_SUICIDE_AFTER_NEUTRAL_CASTLE_ATTACK = document
+	    .getElementById('SOLDIER_SUICIDE_AFTER_NEUTRAL_CASTLE_ATTACK').checked;
+	WIN_AGAINST_NEUTRAL_CASTLE_PROPABILITY = parseFloat(document
+	    .getElementById('WIN_AGAINST_NEUTRAL_CASTLE_PROPABILITY').value);
+	WIN_AGAINST_ENEMY_CASTLE_PROPABILITY = parseFloat(document
+	    .getElementById('WIN_AGAINST_ENEMY_CASTLE_PROPABILITY').value);
+	WIN_AGAINST_ENEMY_SOLDIER_PROPABILITY = parseFloat(document
+	    .getElementById('WIN_AGAINST_ENEMY_SOLDIER_PROPABILITY').value);
+}
