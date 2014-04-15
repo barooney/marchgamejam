437cce8cb2f01c67d076810eb831f9334ac3d733
Daniel Baron <daniel@barooney.de>
2014-04-15 21:16:19 +0200

    added cordova.exec stubs

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/daniel/Projekte/marchgamejam/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/Users/daniel/Projekte/marchgamejam/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -1,12 +1,13 @@
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes.dex : \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/BuildConfig.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/HelloWorld.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/R$attr.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/R$drawable.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/R$string.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/R$xml.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/helloworld/R.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/PGLowLatencyAudio.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/PGLowLatencyAudioAsset.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/classes/com/phonegap/PGPolyphonicVoice.class \
-/Users/daniel/Projekte/marchgamejam_app/platforms/android/bin/dexedLibs/classes-5948e6a44948d1ba4518c8f603845d28.jar \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes.dex : \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/com/phonegap/helloworld/HelloWorld.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/com/phonegap/PGLowLatencyAudio.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/com/phonegap/PGLowLatencyAudioAsset.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/com/phonegap/PGPolyphonicVoice.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/HelloWorld.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/R$attr.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/R$drawable.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/R$string.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/R$xml.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/classes/io/cordova/hellocordova/R.class \
+/Users/daniel/Projekte/marchgamejam/platforms/android/bin/dexedLibs/classes-698b1e9cb49cb8f1dddb41ba22c68933.jar \
